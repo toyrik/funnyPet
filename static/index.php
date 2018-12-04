cc2ca565720754fef5bb84ab9c1c4bfc678edc2e
toyrik <toyrik@gmail.com>
2018-12-04 16:34:17 +0300

    init commit

---
+++ static/index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="ru-RU">
+  <head>
+    <meta charset="utf-8"/>
+    <title>My funny pets - Главная</title>
+    <meta http-equiv="X-UA-Compatible" content="IE = edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta name="description" content=""/>
+    <link rel="apple-touch-icon" sizes="180x180" href="/img/icons/apple-touch-icon.png"/>
+    <link rel="icon" type="image/png" sizes="32x32" href="/img/icons/favicon-32x32.png"/>
+    <link rel="icon" type="image/png" sizes="16x16" href="/img/icons/favicon-16x16.png"/>
+    <link rel="manifest" href="/img/icons/site.webmanifest"/>
+    <link rel="mask-icon" href="/img/icons/safari-pinned-tab.svg" color="#5bbad5"/>
+    <link rel="shortcut icon" href="/img/icons/favicon.ico"/>
+    <meta name="msapplication-TileColor" content="#da532c"/>
+    <meta name="msapplication-config" content="/img/icons/browserconfig.xml"/>
+    <meta name="theme-color" content="#ffffff"/>
+    <link rel="stylesheet" type="text/css" href="./css/style.css"/>
+  </head>
+  <body>
+    <header class="main-header">
+      <div class="wrapper">
+        <div class="header__inner"><a class="logo" href="#"><img class="logo__img" src="./img/svg/logo.svg" alt="logo"/>
+            <h1 class="logo__title">Мои забавные питомцы</h1></a>
+          <nav class="page-nav">
+            <ul class="page-nav__items">
+              <li class="page-nav__item"><a class="page-nav__link" href="#">Мои питомцы</a></li>
+              <li class="page-nav__item"><a class="page-nav__link" href="#">Галарея</a></li>
+              <li class="page-nav__item"><a class="page-nav__link" href="#">Контакты</a></li>
+            </ul>
+            <div class="page-nav__toggle"></div>
+          </nav>
+        </div>
+      </div>
+    </header>
+    <div class="hero">
+      <div class="wrapper">
+        <div class="wrapper__inner">
+          <div class="hero-title">
+            <h1>Realy big title</h1>
+            <h2>Simple, yet engaging and descreptive subtile</h2>
+          </div><img class="hero__img" src="./img/fb-pets.png"/>
+        </div>
+      </div>
+    </div>
+    <section class="main-content"></section>
+    <section class="aside"></section>
+    <footer class="main-footer">
+      <div class="wrapper">
+        <div class="wrapper__inner">
+          <nav class="page-nav">
+            <ul class="page-nav__items">
+              <li class="page-nav__item"><a class="page-nav__link" href="#">О проекте</a></li>
+              <li class="page-nav__item"><a class="page-nav__link" href="#">Сервисы</a></li>
+              <li class="page-nav__item"><a class="page-nav__link" href="#">Контакты</a></li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </footer>
+    <script src="./libs/jquery.min.js"></script>
+    <script src="./js/common.js"></script>
+  </body>
+</html>
